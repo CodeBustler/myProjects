30dfe100c934569845233d9c3401bc70105a4a8b
Mallikarjun M <110022096+CodeBustler@users.noreply.github.com>
2023-03-01 14:32:59 +0530

    Updated Files Added

---
+++ index.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+  document.getElementsByClassName('loader')[0].style.display = 'none';
+  // document.getElementsByClassName('all-projects-container')[0].style.display = 'block';
+}, 4000);
