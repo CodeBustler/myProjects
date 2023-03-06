4ff0c86b7c054dfacee47fc50888235be65c161e
Mallikarjun M <110022096+CodeBustler@users.noreply.github.com>
2023-03-06 14:02:06 +0530

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-setTimeout(() => {
-  document.getElementsByClassName('loader')[0].style.display = 'none';
-  // document.getElementsByClassName('all-projects-container')[0].style.display = 'block';
-}, 4000);
